6e67c53a1f9763f8067f56259e094d735ea560db
Tejas Pancholi <tejas.er@outlook.com>
2013-11-19 10:59:35 +0530

    Initialize repository

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+EffortTracker::Application.config.secret_key_base = '86bb082594e314b15ac210e131b49ab434b0ce4d7f2a4423c5089a23cae1ac9203bed348b67f3f19d883328a4a0deda5b97adcb2dfdcb8de0326f60ba648f6ce'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+EffortTracker::Application.config.session_store :cookie_store, key: '_effort_tracker_session'
